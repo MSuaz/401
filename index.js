9880da86df25ec544d97a5881839b5e744e019f5
EPPR @ LXPS <lxps@eppr.link>
2021-10-14 22:19:04 +0000

    Archivo actualizado para tarea

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-// EPPR Clase 401 : Archivo con las respuestas
+// EPPR Clase 401 : Archivo del branch TAREA.
+// NOMBRE: ⬅️
+// FECHA:  ⬅️
+
 // Definición de una constante con una función anónima.
 const getStatus = (name, age, online) => `${name} is ${age} years old and is ${(!!online)?'now':'NOT'} online`
+
 // Definir 3 Variables de tipo Número, Cadena de Texto y Booleanos.
-var age = 33; // Esta variable debe ser un Número
-var name = "Mr. Dr. Bean"; // Esta variable debe ser una Cadena de Texto
-var online = true; // Esta variable debe ser un Booleano (Verdadero o Falso)
+var age = ; // ⬅️  Aquí poner una variable de tipo Número
+var name = ; // ⬅️ Aquí poner una variable de tipo Cadena de Texto
+var online = ; // ⬅️ Aquí poner una variable de tipo Booleano.
+
 // Programa Principal.
 console.log('------ Running Program --------')
 printTypeOf(age) // El resultado debe ser "number"
 printTypeOf(name) // El resultado debe ser "string"
 printTypeOf(online) // El resultado debe ser "boolean"
 console.log('------ Running getStatus() --------')
-console.log( getStatus(name, age, online) )
+// Aquí escribe 🔽 las variables que pasarás como argumentos.
+console.log( getStatus( ___________ ) )
 // Definición de otras funciones personales.
-function printTypeOf( value ){
+// Escribe 🔽 el nombre correcto de esta función. (Observa la línea 17 👀)
+function _______( value ){
     console.log( `Value: "${value}"` )
     console.log( `Type of: ${typeof(value)}` )
 }
